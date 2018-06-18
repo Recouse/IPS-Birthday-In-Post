3d3b7193ea1b3d95913b6a41dd99cbda851b3c67
Firdavs Khaydarov <recouse@mail.ru>
2018-06-18 20:07:40 +0500

    Revert "fixed merge conflicts"
    This reverts commit 6bf478324cb27f301b0daa3ad92aa5baa4fc0e3c, reversing
    changes made to 7c86aa0b18f2f85fb1fd8a4cf0224e9a139818a8.

--- README.md
+++ README.md
@@ -3,18 +3,9 @@
 This plugin shows a cake icon on the users posts when they have a birthday.
 
 ## Information
-<<<<<<< HEAD
 * Compitability: IPS 4.3.x
 * Author: [**Firdavs Khaydarov**](https://recouse.github.io).
 * Copyright: Firdavs Khaydarov 2018
 
 ## License
 GNU GPLv3 [LICENSE](LICENSE).
-=======
-* Compitability: IPS 4.2.x
-* Author: [**Recouse**](https://recouse.github.io).
-* Copyright: Recouse 2018
-
-## License
-GNU GPLv3 [LICENSE](LICENSE).
->>>>>>> 01fbfe311fa359128ae9e833adf331423f6615e2
--- settings.php
+++ settings.php
@@ -1,16 +1,9 @@
 //<?php
 
-<<<<<<< HEAD
 $form->add(new \IPS\Helpers\Form\Select( 'birthdayInPost_area', \IPS\Settings::i()->birthdayInPost_area, false, array( 'options' => array(
 	'infoList' => "birthdayInPost_infoList",
 	'username' => "birthdayInPost_username"
 ) ) ) );
-=======
-	$form->add(new \IPS\Helpers\Form\Select( 'birthdayInPost_area', \IPS\Settings::i()->birthdayInPost_area, false, array( 'options' => array(
-		'infoList' => "birthdayInPost_infoList",
-		'username' => "birthdayInPost_username"
-	) ) ) );
->>>>>>> 01fbfe311fa359128ae9e833adf331423f6615e2
 
 if ( $values = $form->values() )
 {
